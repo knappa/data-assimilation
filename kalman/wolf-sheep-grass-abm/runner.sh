3f61824b479ee96ef714acc0d68028ad710f36da
Adam Knapp <adam.knapp@instanton.org>
2023-12-01 17:29:44 -0500

    add new cmdline param

--- kalman/wolf-sheep-grass-abm/runner.sh
+++ kalman/wolf-sheep-grass-abm/runner.sh
@@ -2,35 +2,35 @@
 
 for i in $(seq -f "%03g" 0 100)
 do
-  python ekf.py --prefix $i-grass --measurements grass
+  python ekf.py --prefix $i-grass --measurements grass --matchmaker yes
 done
 
 for i in $(seq -f "%03g" 0 100)
 do
-  python ekf.py --prefix $i-sheep --measurements sheep
+  python ekf.py --prefix $i-sheep --measurements sheep --matchmaker yes
 done
 
 for i in $(seq -f "%03g" 0 100)
 do
-  python ekf.py --prefix $i-wolf --measurements wolves
+  python ekf.py --prefix $i-wolf --measurements wolves --matchmaker yes
 done
 
 for i in $(seq -f "%03g" 0 100)
 do
-  python ekf.py --prefix $i-wolfgrass --measurements wolves+grass
+  python ekf.py --prefix $i-wolfgrass --measurements wolves+grass --matchmaker yes
 done
 
 for i in $(seq -f "%03g" 0 100)
 do
-  python ekf.py --prefix $i-wolfsheep --measurements wolves+sheep
+  python ekf.py --prefix $i-wolfsheep --measurements wolves+sheep --matchmaker yes
 done
 
 for i in $(seq -f "%03g" 0 100)
 do
-  python ekf.py --prefix $i-sheepgrass --measurements sheep+grass
+  python ekf.py --prefix $i-sheepgrass --measurements sheep+grass --matchmaker yes
 done
 
 for i in $(seq -f "%03g" 0 100)
 do
-  python ekf.py --prefix $i-wolfsheepgrass --measurements wolves+sheep+grass
+  python ekf.py --prefix $i-wolfsheepgrass --measurements wolves+sheep+grass --matchmaker yes
 done
