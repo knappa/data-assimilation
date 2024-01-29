0a3de44d8af079a99a5484c3e0b57a2708ba2958
Adam Knapp <adam.knapp@instanton.org>
2024-01-29 13:22:19 -0500

    all vars synthesis

--- kalman/an-cockrell-abm/modify_full_spatial.py
+++ kalman/an-cockrell-abm/modify_full_spatial.py
@@ -1,10 +1,10 @@
-import itertools
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Tuple
 
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
 from an_cockrell import AnCockrellModel, EndoType, EpiType, epitype_one_hot_encoding
+from matplotlib import animation
 from scipy.optimize import Bounds, OptimizeResult, lsq_linear
 
 from util import compute_desired_epi_counts, smooth_random_field
@@ -207,14 +207,14 @@ quantizer = quantization_maker(
 def dither(
     model: AnCockrellModel,
     new_epi_counts,
-) -> np.ndarray:
+) -> Tuple[np.ndarray, animation.FuncAnimation]:
     # store spatial distribution of one-hot encoding for epithelial cell type
     state_vecs = np.zeros(shape=(*model.geometry, block_dim), dtype=np.float64)
     state_vecs[:, :, : len(EpiType)] = epitype_one_hot_encoding(model.epithelium)
     for idx, spatial_var in enumerate(spatial_vars, start=len(EpiType)):
         state_vecs[:, :, idx] = getattr(model, spatial_var).astype(np.float64)
 
-    fig = plt.figure(constrained_layout=True, figsize=(10, 4))
+    fig = plt.figure(constrained_layout=True, figsize=(10, 12))
     gs = fig.add_gridspec(7, 5)
     # categories
     axs = [fig.add_subplot(gs[0, 0:2]), fig.add_subplot(gs[0, 3:5])]
@@ -223,11 +223,13 @@ def dither(
         axs.append(fig.add_subplot(gs[1, plt_idx]))
     # spatial vars
     for plt_idx in range(len(spatial_vars)):
-        row_idx = 2+(plt_idx // 5)
+        row_idx = 2 + (plt_idx // 5)
         col_idx = plt_idx % 5
         axs.append(fig.add_subplot(gs[row_idx, col_idx]))
     # orig_cat_plot =
-    axs[0].imshow(np.argmax(state_vecs[:, :, : len(EpiType)], axis=2), vmin=0, vmax=max(EpiType))
+    axs[0].imshow(
+        np.argmax(state_vecs[:, :, : len(EpiType)], axis=2), vmin=0, vmax=max(EpiType)
+    )
 
     # counts of epi cell types for the incoming model
     prev_epi_count = np.sum(state_vecs[:, :, : len(EpiType)], axis=(0, 1), dtype=int)
@@ -252,7 +254,8 @@ def dither(
         np.argmax(state_vecs[:, :, : len(EpiType)], axis=2), vmin=0, vmax=4
     )
     state_plots = [
-        axs[idx + 2].imshow(np.abs(state_vecs[:, :, idx]), vmin=0, vmax=1.5) for idx in range(5)
+        axs[idx + 2].imshow(np.abs(state_vecs[:, :, idx]), vmin=0, vmax=1.5)
+        for idx in range(5)
     ]
     axs[0].set_title("Orig Category")
     axs[1].set_title("Category")
@@ -267,15 +270,20 @@ def dither(
         for idx in range(len(spatial_vars))
     ]
     for idx, spatial_var in enumerate(spatial_vars, start=7):
-        axs[idx].set_title(spatial_var)
+        axs[idx].set_title(
+            spatial_var.replace("endothelial_", "endothelial\n").replace("_", " "),
+            loc="center",
+            wrap=True,
+        )
 
-    fig.tight_layout()
+    num_frames = model.geometry[0] * model.geometry[1]
 
     newly_set_epi_counts = np.zeros(len(EpiType), dtype=np.int64)
 
-    for row_idx, col_idx in itertools.product(
-        range(model.geometry[0]), range(model.geometry[1])
-    ):
+    def update(frame_num: int):
+        modulus = model.geometry[1]
+        row_idx, col_idx = divmod(frame_num, modulus)
+
         # compute which epitypes are available for placement, where available means that we have not yet used
         # up all requested instances.
         available_epitypes = [
@@ -292,7 +300,6 @@ def dither(
         newly_set_epi_counts[cell_type] += 1
 
         # TODO: consistency checks for these cell types (internal virus, etc)
-
         error = state_vecs[row_idx, col_idx, :] - new_state
         state_vecs[row_idx, col_idx, :] = new_state
 
@@ -375,21 +382,10 @@ def dither(
             1 - np.sum(state_vecs[row_idx_plus, col_idx_plus, : len(EpiType)])
         ) / len(EpiType)
 
-    return np.argmax(state_vecs, axis=2)
+    # noinspection PyTypeChecker
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=num_frames, interval=30)
 
-
-# from modify_spatial import floyd_steinberg_dither
-#
-# epithelium = np.full((50, 50), EpiType.Healthy, dtype=EpiType)
-# X, Y = np.meshgrid(np.arange(50), np.arange(50))
-# epithelium[np.sqrt((X - 25) ** 2 + (Y - 25) ** 2) < 10] = EpiType.Infected
-# epithelium[np.sqrt((X - 25) ** 2 + (Y - 50) ** 2) < 10] = EpiType.Empty
-# empty_count, healthy_count, infected_count, dead_count, apoptosed_count = [
-#     np.sum(epithelium.astype(int) == tp) for tp in range(5)
-# ]
-# new_epithelium = floyd_steinberg_dither(
-#     epithelium, healthy_count, infected_count, apoptosed_count, dead_count, empty_count
-# )
+    return np.argmax(state_vecs, axis=2), ani
 
 
 def modify_model(
@@ -509,7 +505,7 @@ def modify_model(
     ################################################################################
 
     fig, axs = plt.subplots(2)
-    dither_result = dither(
+    dither_result, ani = dither(
         model, compute_desired_epi_counts(desired_state, model, state_var_indices)
     )
     axs[0].imshow(model.epithelium.astype(int), vmin=0, vmax=4)
