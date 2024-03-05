0a0439073efba757b5f5f2eeb19c08adce17220a
Adam Knapp <adam.knapp@instanton.org>
2024-03-05 16:26:25 -0500

    update plotted stats

--- kalman/wolf-sheep-grass-abm/stats.py
+++ kalman/wolf-sheep-grass-abm/stats.py
@@ -1,51 +1,89 @@
 #!/usr/bin/env python3
+# Surprisal plots from bulk runs
+import os
 
-import csv
-from collections import defaultdict
-
-import matplotlib.pyplot as plt
+import h5py
 import numpy as np
+from matplotlib import pyplot as plt
 
-categories = ["grass", "sheep", "wolf", "wolfgrass", "wolfsheep", "sheepgrass", "wolfsheepgrass"]
+types = [
+    ("g", "grass"),
+    ("s", "sheep"),
+    ("w", "wolves"),
+    ("sg", "sheep, grass"),
+    ("wg", "wolves, grass"),
+    ("ws", "wolves, sheep"),
+    ("wsg", "wolves, sheep, grass"),
+]
 
-full_surprisal_stats = defaultdict(list)
-state_surprisal_stats = defaultdict(list)
-param_surprisal_stats = defaultdict(list)
+surprisal_full = dict()
+surprisal_state = dict()
+surprisal_param = dict()
 
-for category in categories:
-    for idx in range(101):
-        try:
-            filename = f"{idx:03}-{category}-meansurprisal.csv"
-            with open(filename, "r") as file:
-                csvreader = csv.reader(file)
-                headers = next(csvreader)
-                full, state, param = map(float, next(csvreader))
-                full_surprisal_stats[category].append(full)
-                state_surprisal_stats[category].append(state)
-                param_surprisal_stats[category].append(param)
-            print(filename)
-        except FileNotFoundError:
-            pass
+for short_name, name in types:
+    files = [
+        f
+        for f in os.listdir(".")
+        if os.path.isfile(f) and f[-5:] == ".hdf5" and f[: len(short_name) + 1] == short_name + "-"
+    ]
+    # get shapes
+    with h5py.File(files[0], "r") as h5file:
+        full_shape = h5file["surprisal_full"][()].shape
+        state_shape = h5file["surprisal_state"][()].shape
+        param_shape = h5file["surprisal_param"][()].shape
+    # init arrays
+    surprisal_full[short_name] = np.zeros((len(files), *full_shape), dtype=np.float64)
+    surprisal_state[short_name] = np.zeros((len(files), *state_shape), dtype=np.float64)
+    surprisal_param[short_name] = np.zeros((len(files), *param_shape), dtype=np.float64)
+    # collect data
+    for file_idx, file in enumerate(files):
+        with h5py.File(file, "r") as h5file:
+            surprisal_full[short_name][file_idx] = h5file["surprisal_full"][()]
+            surprisal_state[short_name][file_idx] = h5file["surprisal_state"][()]
+            surprisal_param[short_name][file_idx] = h5file["surprisal_param"][()]
 
-np_full_surprisal_stats = dict()
-np_state_surprisal_stats = dict()
-np_param_surprisal_stats = dict()
+fig = plt.figure()
+ax = fig.gca()
+for short_name, name in types:
+    ax.plot(
+        [
+            np.mean(np.mean(surprisal_full[short_name], axis=0)[idx, 50 * idx : 50 * (idx + 1)])
+            for idx in range(20)
+        ],
+        label=name,
+    )
+fig.suptitle("Full surprisal")
+fig.legend()
+fig.savefig("surp-full.pdf")
+plt.close(fig)
 
-for category in categories:
-    np_full_surprisal_stats[category] = np.array(full_surprisal_stats[category])
-    np_state_surprisal_stats[category] = np.array(state_surprisal_stats[category])
-    np_param_surprisal_stats[category] = np.array(param_surprisal_stats[category])
 
-del full_surprisal_stats
-del state_surprisal_stats
-del param_surprisal_stats
+fig = plt.figure()
+ax = fig.gca()
+for short_name, name in types:
+    ax.plot(
+        [
+            np.mean(np.mean(surprisal_state[short_name], axis=0)[idx, 50 * idx : 50 * (idx + 1)])
+            for idx in range(20)
+        ],
+        label=name,
+    )
+fig.suptitle("State surprisal")
+fig.legend()
+fig.savefig("surp-state.pdf")
+plt.close(fig)
 
-stat = np_full_surprisal_stats
-upper_cutoff = 200
-for category in categories:
-    stat_cat = stat[category]
-    plt.hist(stat_cat[stat_cat <= upper_cutoff], label=category, density=True)
-plt.title("Surprisal")
-plt.legend()
-plt.tight_layout()
-plt.show()
+fig = plt.figure()
+ax = fig.gca()
+for short_name, name in types:
+    ax.plot(
+        [
+            np.mean(np.mean(surprisal_param[short_name], axis=0)[idx, 50 * idx : 50 * (idx + 1)])
+            for idx in range(20)
+        ],
+        label=name,
+    )
+fig.suptitle("Parameter surprisal")
+fig.legend()
+fig.savefig("surp-param.pdf")
+plt.close(fig)
