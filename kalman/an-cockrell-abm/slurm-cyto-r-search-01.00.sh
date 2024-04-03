b5b742953c70ed5f6562fcfabb9844411deae9ba
Adam Knapp <adam.knapp@instanton.org>
2024-04-03 10:28:54 -0400

    add slurm scripts for r-tuning

---
+++ kalman/an-cockrell-abm/slurm-cyto-r-search-01.00.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#SBATCH --job-name=c01.00                    # Job name
+#SBATCH --mail-type=ALL                     # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=adam.knapp@ufl.edu      # Where to send mail
+#SBATCH --nodes=1                           # Use one node (non-MPI)
+#SBATCH --ntasks=1                          # Run a single task (non-MPI)
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=8gb                   # Memory per job
+#SBATCH --time=72:00:00                     # Time limit hrs:min:sec
+#SBATCH --output=c01.00-array_%A-%a.out      # Standard output and error log
+#SBATCH --array=0-100                       # Array range
+# This is an example script that combines array tasks with
+# bash loops to process many short runs. Array jobs are convenient
+# for running lots of tasks, but if each task is short, they
+# quickly become inefficient, taking more time to schedule than
+# they spend doing any work and bogging down the scheduler for
+# all users. 
+pwd; hostname; date
+
+#Set the number of runs that each SLURM task should do
+PER_TASK=1
+
+# Calculate the starting and ending values for this task based
+# on the SLURM task and the number of runs per task.
+START_NUM=$(( SLURM_ARRAY_TASK_ID * PER_TASK ))
+END_NUM=$(( (SLURM_ARRAY_TASK_ID + 1) * PER_TASK ))
+
+# Print the task and run range
+echo "This is task $SLURM_ARRAY_TASK_ID, which will do runs $START_NUM to $(( END_NUM - 1 ))"
+
+module load python3
+cd /home/adam.knapp/blue_rlaubenbacher/adam.knapp/data-assimilation/kalman/an-cockrell-abm || exit
+source venv/bin/activate
+
+
+# Run the loop of runs for this task.
+for (( run=START_NUM; run < END_NUM; run++ )); do
+  echo "This is SLURM task $SLURM_ARRAY_TASK_ID, run number $run"
+  #Do your stuff here
+
+  PREFIX=cyt-01.00-$(printf %04d $run)
+
+  python3 ekf.py \
+  --measurements P_DAMPS T1IFN TNF IFNg IL1 IL6 IL8 IL10 IL12 IL18 \
+  --update-algorithm spatial \
+  --param_stoch_level 0.001 \
+  --uncertainty-P_DAMPS 1.0 \
+  --uncertainty-T1IFN 1.0 \
+  --uncertainty-TNF 1.0 \
+  --uncertainty-IFNg 1.0 \
+  --uncertainty-IL6 1.0 \
+  --uncertainty-IL1 1.0 \
+  --uncertainty-IL8 1.0 \
+  --uncertainty-IL10 1.0 \
+  --uncertainty-IL12 1.0 \
+  --uncertainty-IL18 1.0 \
+  --grid_width 51 \
+  --grid_height 51 \
+  --prefix $PREFIX \
+  --predict to-end \
+  # --graphs \
+  # --verbose
+
+done
+
+date
+
+
