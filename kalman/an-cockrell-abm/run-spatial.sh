650dad3abee683a5ce784ef612d9c36f8728768c
Adam Knapp <adam.knapp@instanton.org>
2024-04-03 18:54:52 -0400

    match run-simple

--- kalman/an-cockrell-abm/run-spatial.sh
+++ kalman/an-cockrell-abm/run-spatial.sh
@@ -3,19 +3,19 @@
 ./ekf.py \
   --measurements P_DAMPS T1IFN TNF IFNg IL1 IL6 IL8 IL10 IL12 IL18 extracellular_virus \
   --update-algorithm spatial \
-  --uncertainty-P_DAMPS 0.01 \
-  --uncertainty-T1IFN 0.01 \
-  --uncertainty-TNF 0.01 \
-  --uncertainty-IFNg 0.01 \
-  --uncertainty-IL6 0.01 \
-  --uncertainty-IL1 0.01 \
-  --uncertainty-IL8 0.01 \
-  --uncertainty-IL10 0.01 \
-  --uncertainty-IL12 0.01 \
-  --uncertainty-IL18 0.01 \
+  --uncertainty-P_DAMPS 0.001 \
+  --uncertainty-T1IFN 0.001 \
+  --uncertainty-TNF 0.001 \
+  --uncertainty-IFNg 0.001 \
+  --uncertainty-IL6 0.001 \
+  --uncertainty-IL1 0.001 \
+  --uncertainty-IL8 0.001 \
+  --uncertainty-IL10 0.001 \
+  --uncertainty-IL12 0.001 \
+  --uncertainty-IL18 0.001 \
   --graphs \
   --grid_width 51 \
   --grid_height 51 \
-  --prefix full \
+  --prefix spatial \
   --predict to-next-kf-update \
   # --verbose
