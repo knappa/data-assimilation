3cc3ff6661bef12ee9f55b19b49b598924c54f05
Adam Knapp <adam.knapp@instanton.org>
2025-03-06 14:33:35 -0500

    begin a refactoring of the KF code into a class

--- phenotype-kalman/an-cockrell-abm/phkf_ac/__init__.py
+++ phenotype-kalman/an-cockrell-abm/phkf_ac/__init__.py
@@ -1,15 +1,82 @@
-from typing import List, Optional, Callable
+from typing import List
 
-import an_cockrell
+import numpy as np
 from an_cockrell import AnCockrellModel
-import attrs
-from attrs import define, field, Factory
+from attrs import Factory, define, field
 
-@define
-class PhenotypeKFAnCockrell():
 
-    phenotype_weight_function : List[Callable] = field(init=True)
+@define(kw_only=True)
+class PhenotypeKFAnCockrell:
+    num_phenotypes: int = field(init=True)
+
+    # PCA from 2017*41 dimensions to 3 by matrix C (3,2017*41)
+    # then each phenotype_i from gaussian with mean m_i (3,) and cov P_i (3,3)
+    # -> exp( -(1/2) (Cx-m_i)^T P_i^{-1} (Cx-m_i) )
+    pca_matrix: np.ndarray = field(init=True)
+    phenotype_weight_means: np.ndarray = field(init=True)
+    phenotype_weight_covs: np.ndarray = field(init=True)
+
+    # initial distribution
+    phenotype_distribution: np.ndarray = field()
+    per_phenotype_means: np.ndarray = field(init=True)
+    per_phenotype_covs: np.ndarray = field(init=True)
+
+    # noinspection PyUnresolvedReferences
+    @phenotype_distribution.default
+    def phenotype_distribution_default(self):
+        return np.full(self.num_phenotypes, 1 / self.num_phenotypes, dtype=np.float64)
 
     ensemble: List[AnCockrellModel] = Factory(list)
+    ensemble_projection: np.ndarray = field(init=False)
 
-    pass
+    # noinspection PyUnresolvedReferences
+    @ensemble_projection.default
+    def ensemble_projection_default(self):
+        return np.zeros(0, dtype=np.float64)
+
+    initial_time : int = -1
+
+    def __attrs_post_init__(self):
+        assert (
+            self.num_phenotypes
+            == self.phenotype_weight_means.shape[0]
+            == self.phenotype_weight_covs.shape[0]
+            == self.phenotype_distribution.shape[0]
+        ), "mismatch in number of phenotypes"
+        assert (
+            self.phenotype_weight_means.shape[1]
+            == self.phenotype_weight_covs.shape[1]
+            == self.phenotype_weight_covs.shape[2]
+        ), "dimension mismatch"
+
+        # TODO: if the per_phenotype_means/covs is only for 1 phenotype, expand it to all N equally
+
+    def initialize_ensemble(self, *, t: int = 0) -> None:
+        """
+        Initialize the ensemble.
+
+        :param t: initial time
+        :return:
+        """
+        # TODO: initialization uses different parameters than the running state (Ugh) what to do here?
+        pass
+
+    def project_to(self, *, t: int) -> None:
+        """
+        Project the ensemble to time t.
+        :param t: time
+        :return: None
+        """
+        # TODO: decide how much of the ensemble to save (summary/microstate, for how many time steps, etc.)
+        pass
+
+    def kf_update(self, *, t: int, types: List[str], measurements: np.ndarray) -> None:
+        """
+
+        :param t: time
+        :param types: list of measured quantities
+        :param measurements: values of measured quantities
+        :return:
+        """
+        # TODO: check the time
+        pass
